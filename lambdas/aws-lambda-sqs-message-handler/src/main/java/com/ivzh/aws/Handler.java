abbf1c84b10619aaaf349e8b17de5e1e217f5a5f
Ivan Zhuravel <ivanguravel@gmail.com>
2022-11-14 20:28:09 +0200

    add dynamo db support

--- lambdas/aws-lambda-sqs-message-handler/src/main/java/com/ivzh/aws/Handler.java
+++ lambdas/aws-lambda-sqs-message-handler/src/main/java/com/ivzh/aws/Handler.java
@@ -1,17 +1,43 @@
 package com.ivzh.aws;
 
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
+import com.amazonaws.services.dynamodbv2.document.DynamoDB;
+import com.amazonaws.services.dynamodbv2.document.Item;
+import com.amazonaws.services.dynamodbv2.document.Table;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.SQSEvent;
 import com.amazonaws.services.lambda.runtime.events.SQSEvent.SQSMessage;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
-public class Handler implements RequestHandler<SQSEvent, Void>{
+import java.util.HashMap;
+import java.util.Map;
+
+public class Handler implements RequestHandler<SQSEvent, Void> {
+
+    private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
+
     @Override
-    public Void handleRequest(SQSEvent event, Context context)
-    {
-        for(SQSMessage msg : event.getRecords()){
-            System.out.println(msg.getBody());
+    public Void handleRequest(SQSEvent event, Context context) {
+        Map<String, String> map = new HashMap<>();
+        for (SQSMessage msg : event.getRecords()) {
+            map.putAll(msg.getAttributes());
         }
+
+        AmazonDynamoDB client = AmazonDynamoDBClientBuilder.standard().build();
+        DynamoDB dynamoDB = new DynamoDB(client);
+
+        Table table = dynamoDB.getTable("books-metadata");
+        Item item = null;
+        try {
+            item = new Item().withJSON("data",OBJECT_MAPPER.writeValueAsString(map))
+                    .withPrimaryKey("book", "1");
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+        table.putItem(item);
         return null;
     }
 }
