409ae9e3773565fc78272dd0852f37f1efbe0c21
izhuravel <ivanguravel@gmail.com>
2022-12-02 22:52:57 +0200

    add function for dynamodb

---
+++ lambdas/aws-lambda-sqs-dynamo-db-handler/src/main/java/com/ivzh/aws/DynamoDbPayloadHandler.java
@@ -0,0 +1,55 @@
+package com.ivzh.aws;
+
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
+import com.amazonaws.services.dynamodbv2.document.DynamoDB;
+import com.amazonaws.services.dynamodbv2.document.Item;
+import com.amazonaws.services.dynamodbv2.document.Table;
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.SQSEvent;
+import com.amazonaws.services.lambda.runtime.events.SQSEvent.SQSMessage;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class DynamoDbPayloadHandler implements RequestHandler<SQSEvent, Void> {
+
+    private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
+
+    private final DynamoDB dynamoDB;
+
+    public DynamoDbPayloadHandler() {
+        AmazonDynamoDB client = AmazonDynamoDBClientBuilder.standard().build();
+        this.dynamoDB = new DynamoDB(client);
+    }
+
+    @Override
+    public Void handleRequest(SQSEvent event, Context context) {
+        Map<String, String> map = new HashMap<>();
+
+        for (SQSMessage msg : event.getRecords()) {
+            map.putAll(msg.getAttributes());
+        }
+
+        Table table = dynamoDB.getTable("books-metadata");
+        table.putItem(createItem(map));
+        return null;
+    }
+
+    private Item createItem(Map<String, String> parameters) {
+        return new Item()
+                .withJSON("data", safeConvertObjectToJson(parameters))
+                .withPrimaryKey("book", "1");
+    }
+
+    private String safeConvertObjectToJson(Object o) {
+        try {
+            return OBJECT_MAPPER.writeValueAsString(o);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
